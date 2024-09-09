f1d8f4e4c783a55e20c3e05f28565a4814fa18f3
ColinAdo <colinadodev30@gmail.com>
2024-09-09 12:54:23 +0300

    Add fstyled home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Libo | Home",
@@ -6,5 +7,36 @@ export const metadata: Metadata = {
 };
 
 export default function Page() {
-  return <h2>Home page</h2>;
+  return (
+    <main className="bg-white">
+      <div className="relative isolate px-6 pt-14 lg:px-8">
+        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+          <div className="text-center">
+            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+              Libo application
+            </h1>
+            <p className="mt-6 text-lg leading-8 text-gray-600">
+              This is a library application that allows you to react and chat
+              with your pdf, after somedays the file will be sent into your
+              email address.
+            </p>
+            <div className="mt-10 flex items-center justify-center gap-x-6">
+              <Link
+                href="/auth/register"
+                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Get started
+              </Link>
+              <Link
+                href="/auth/login"
+                className="text-sm font-semibold leading-6 text-gray-900"
+              >
+                Login <span aria-hidden="true">&rarr;</span>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
