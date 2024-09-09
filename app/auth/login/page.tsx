7a573787effbac487a6b5c777255a54a5e41cad1
ColinAdo <colinadodev30@gmail.com>
2024-09-09 09:57:51 +0300

    Add metadata

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,3 +1,10 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Libo | Login",
+  description: "Libo login page",
+};
+
 export default function Page() {
   return <h2>Login</h2>;
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Libo | Home",
+  description: "Libo home page",
+};
+
 export default function Page() {
   return <h2>Home page</h2>;
 }
