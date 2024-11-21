2c9040e03f0f5aba526acffc4a65c5bd5b64f38f
ColinAdo <colinadodev30@gmail.com>
2024-11-21 07:33:14 +0300

    Updated password reset confirm route

--- app/password-reset/[uid]/[token]/page.tsx
+++ app/password-reset/[uid]/[token]/page.tsx
@@ -1,4 +1,5 @@
-import { PasswordResetConfirmForm } from "@/components/forms";
+import PasswordResetConfirmForm from "@/components/auth/PasswordResetConfirmForm";
+import { SquareLibrary } from "lucide-react";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -16,14 +17,10 @@ interface Props {
 export default function Page({ params: { uid, token } }: Props) {
   return (
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <img
-          alt="Libo"
-          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-          className="mx-auto h-10 w-auto"
-        />
-        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-          Reset your password
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm mb-0">
+        <SquareLibrary className="mx-auto h-10 w-auto" />
+        <h2 className="mt-1 text-center dark:text-white text-2xl font-mono font-bold leading-9 tracking-tight text-gray-900">
+          Libo
         </h2>
       </div>
 
