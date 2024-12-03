62fa93a64f5d4c508fb38a67862cfa21989dfefc
ColinAdo <colinadodev30@gmail.com>
2024-12-03 04:38:48 +0300

    Add proper import

--- redux/hooks.ts
+++ redux/hooks.ts
@@ -1,6 +1,6 @@
+import type { RootStore, AppDispatch } from "./store";
 import { useSelector, useDispatch } from "react-redux";
 import type { TypedUseSelectorHook } from "react-redux";
-import type { RootStore, AppDispatch } from "./store";
 
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootStore> = useSelector;
