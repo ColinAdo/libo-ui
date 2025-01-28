f8c4c5b36388d25f07a703bf706d8d4f0443b88d
ColinAdo <colinadodev30@gmail.com>
2025-01-28 11:09:40 +0300

    Add proper import

--- components/auth/PasswordResetConfirmForm.tsx
+++ components/auth/PasswordResetConfirmForm.tsx
@@ -7,10 +7,10 @@ import { usePasswordResetConfirm } from "@/hooks";
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 import {
   Form,
-  FormControl,
-  FormField,
   FormItem,
+  FormField,
   FormLabel,
+  FormControl,
   FormMessage,
 } from "@/components/ui/form";
 
