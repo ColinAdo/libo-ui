e5dda6632547d817365032ba838545ac4e052160
ColinAdo <colinadodev30@gmail.com>
2025-01-31 16:17:30 +0300

    Delete navlink component

--- components/common/index.ts
+++ components/common/index.ts
@@ -2,5 +2,5 @@ export { default as List } from "./List";
 export { default as Navbar } from "./Navbar";
 export { default as Footer } from "./Footer";
 export { default as Spinner } from "./Spinner";
-export { default as NavLink } from "./NavLink";
+export { default as Sidebar } from "./Sidebar";
 export { default as ThemeToggler } from "./ThemeToggler";
