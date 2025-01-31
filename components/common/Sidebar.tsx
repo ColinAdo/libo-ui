2c796acb0320d592fe40f7bf52bfaabd096633dd
ColinAdo <colinadodev30@gmail.com>
2025-01-31 16:18:35 +0300

    Add sidebar

---
+++ components/common/Sidebar.tsx
@@ -0,0 +1,115 @@
+"use client";
+
+import { Nav } from "@/components/ui/Nav";
+import { useState, useEffect } from "react";
+import { Button } from "@/components/ui/button";
+import { useWindowWidth } from "@react-hook/window-size";
+// import { useGetTransactionsQuery } from "@/redux/features/accountSlice";
+import {
+    Archive,
+    ArchiveX,
+    LayoutDashboard,
+    Trash2,
+    UserRound,
+    ChevronRight,
+    BadgePlus,
+    Brain,
+    BadgeDollarSign,
+    DiamondPlus
+} from "lucide-react";
+
+export default function Sidebar() {
+    // const { data: transactions } = useGetTransactionsQuery();
+    const [isCollapsed, setIsCollapsed] = useState(false);
+    const [mobilewidth, setMobileWidth] = useState(false);
+    const onlyWidth = useWindowWidth();
+
+    useEffect(() => {
+        // Update mobilewidth state when window width changes
+        setMobileWidth(onlyWidth < 768);
+    }, [onlyWidth]);
+
+    function toggleSidebar() {
+        setIsCollapsed(!isCollapsed);
+    }
+
+    return (
+        <div className="relative min-w-[80px] border-r px-3 pt-20 pb-10">
+            {!mobilewidth && (
+                <div className="absolute top-7">
+                    <Button
+                        onClick={toggleSidebar}
+                        className="bg-white text-black hover:bg-slate-300"
+                    >
+                        <ChevronRight />
+                    </Button>
+                </div>
+            )}
+            <Nav
+                isCollapsed={mobilewidth ? true : isCollapsed}
+                links={[
+                    {
+                        title: "Dashboard",
+                        icon: LayoutDashboard,
+                        variant: "default",
+                        href: "/dashboard",
+                    },
+                    {
+                        title: "Transaction",
+                        label: `20`,
+                        icon: BadgeDollarSign,
+                        variant: "ghost",
+                        href: "/dashboard/transactions",
+                    },
+                    {
+                        title: "Create",
+                        icon: BadgePlus,
+                        variant: "ghost",
+                        href: "/dashboard/create/account",
+                    },
+                    {
+                        title: "Transact",
+                        icon: DiamondPlus,
+                        variant: "ghost",
+                        href: "/dashboard/create/transaction",
+                    },
+                    {
+                        title: "Users",
+                        label: "9",
+                        icon: UserRound,
+                        variant: "ghost",
+                        href: "/users",
+                    },
+                    {
+                        title: "Junk",
+                        label: "23",
+                        icon: ArchiveX,
+                        variant: "ghost",
+                        href: "#",
+                    },
+                    {
+                        title: "Trash",
+                        label: "",
+                        icon: Trash2,
+                        variant: "ghost",
+                        href: "#",
+                    },
+                    {
+                        title: "Archive",
+                        label: "",
+                        icon: Archive,
+                        variant: "ghost",
+                        href: "#",
+                    },
+                    {
+                        title: "Ask AI",
+                        label: "",
+                        icon: Brain,
+                        variant: "ghost",
+                        href: "#",
+                    },
+                ]}
+            />
+        </div>
+    );
+}
---
+++ components/ui/Nav.tsx
@@ -0,0 +1,102 @@
+"use client";
+
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { LucideIcon } from "lucide-react";
+import { usePathname } from "next/navigation";
+import { buttonVariants } from "@/components/ui/button";
+// import { useGetAccountsQuery } from "@/redux/features/accountSlice";
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+interface NavProps {
+    isCollapsed: boolean;
+    links: {
+        title: string;
+        label?: string;
+        icon: LucideIcon;
+        variant: "default" | "ghost";
+        href: string;
+    }[];
+}
+
+export function Nav({ links, isCollapsed }: NavProps) {
+    //   const { data: accounts } = useGetAccountsQuery();
+    const pathname = usePathname();
+
+    return (
+        <TooltipProvider>
+            <div
+                data-collapsed={isCollapsed}
+                className={cn(
+                    "group flex flex-col gap-4 py-2",
+                    isCollapsed && "data-[collapsed=true]:py-2"
+                )}
+            >
+                <nav
+                    className={cn(
+                        "grid gap-1 px-2",
+                        isCollapsed && "group-[[data-collapsed=true]]:justify-center group-[[data-collapsed=true]]:px-2"
+                    )}
+                >
+                    {links.map((link, index) => {
+                        const linkContent = (
+                            <>
+                                <link.icon className={cn("mr-2 h-4 w-4", isCollapsed && "h-4 w-4")} />
+                                {!isCollapsed && (
+                                    <>
+                                        {link.title}
+                                        {link.label && (
+                                            <span
+                                                className={cn(
+                                                    "ml-auto",
+                                                    link.variant === "default" && "text-background dark:text-white"
+                                                )}
+                                            >
+                                                {link.label}
+                                            </span>
+                                        )}
+                                    </>
+                                )}
+                            </>
+                        );
+
+                        return (
+                            <Tooltip key={index} delayDuration={0}>
+                                <TooltipTrigger asChild>
+                                    <Link
+                                        href={link.href}
+                                        className={cn(
+                                            buttonVariants({
+                                                variant: link.href === pathname ? "default" : "ghost",
+                                                size: isCollapsed ? "icon" : "sm",
+                                            }),
+                                            "h-9 w-9",
+                                            link.variant === "default" &&
+                                            "dark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white",
+                                            !isCollapsed && "justify-start"
+                                        )}
+                                    >
+                                        {linkContent}
+                                    </Link>
+                                </TooltipTrigger>
+                                {isCollapsed && (
+                                    <TooltipContent side="right" className="flex items-center gap-4">
+                                        {link.title}
+                                        {link.label && (
+                                            <span className="ml-auto text-muted-foreground">{link.label}</span>
+                                        )}
+                                    </TooltipContent>
+                                )}
+                            </Tooltip>
+                        );
+                    })}
+                </nav>
+            </div>
+        </TooltipProvider>
+    );
+}
