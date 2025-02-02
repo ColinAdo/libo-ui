08ecc393cffcd4521650cfcef72def4ace3a23e1
ColinAdo <colinadodev30@gmail.com>
2025-02-02 12:55:28 +0300

    Add profile route

---
+++ app/dashboard/[username]/saved/page.tsx
@@ -0,0 +1,35 @@
+import { ProfileTabs } from "@/components/dashboard";
+import { PostsGrid } from "@/components/dashboard";
+
+const posts = [
+    {
+        id: "1",
+        fileUrl: "/assets/demo.jpeg",
+        likes_count: 40,
+        bookmark_count: 50,
+        description: "description"
+    },
+    {
+        id: "2",
+        fileUrl: "/assets/demo.jpeg",
+        likes_count: 40,
+        bookmark_count: 50,
+        description: "description"
+    },
+    {
+        id: "3",
+        fileUrl: "/assets/demo.jpeg",
+        likes_count: 40,
+        bookmark_count: 50,
+        description: "description"
+    },
+];
+
+export default function Page() {
+    return (
+        <div>
+            <ProfileTabs isCurrentUser={true} username="ado" />
+            <PostsGrid posts={posts} />
+        </div>
+    );
+}
