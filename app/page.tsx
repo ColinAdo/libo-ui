32401977ba555cb5371d6f4f59a3bb4dbbe65b74
ColinAdo <colinadodev30@gmail.com>
2025-02-04 08:28:23 +0300

    Add dark theme style

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
 
 export default function Page() {
   return (
-    <main className="bg-white">
+    <main className="bg-white dark:bg-zinc-950">
       <div className="relative isolate px-6 pt-14 lg:px-8">
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="text-center">
