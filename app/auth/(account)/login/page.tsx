3cb425a3ec4432a896feb2aafedd4ef532f1eada
ColinAdo <colinadodev30@gmail.com>
2025-02-05 12:23:07 +0300

    Add proper style

--- app/auth/(account)/login/page.tsx
+++ app/auth/(account)/login/page.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
 
 export default function Page() {
   return (
-    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+    <div className="flex min-h-full flex-1 flex-col mt-2 justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm mb-0">
         <SquareLibrary className="mx-auto h-10 w-auto" />
         <h2 className="mt-1 text-center text-2xl dark:text-white font-mono font-bold leading-9 tracking-tight text-gray-900">
