0798ed68fa3df1d85d30395450404be12c66cdc4
ColinAdo <colinadodev30@gmail.com>
2025-02-08 09:36:00 +0300

    Add categories route

---
+++ app/dashboard/categories/[id]/page.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+import { useEffect, useState } from "react";
+// import { useWebSocketContext } from "@/hooks/WebSocketContext";
+import { useRetrieveUserQuery } from "@/redux/features/authApiSlice";
+import { PostsGrid, PageTitle } from "@/components/dashboard";
+import { Search } from "lucide-react";
+import { Input } from "@/components/ui/input";
+
+
+const posts = [
+    {
+        id: "1",
+        fileUrl: "/assets/demo.jpeg",
+        likes_count: 40,
+        bookmark_count: 50,
+        description: "Think and Grow Rich"
+    },
+    {
+        id: "2",
+        fileUrl: "/assets/demo.jpeg",
+        likes_count: 40,
+        bookmark_count: 50,
+        description: "Your next five moves"
+    },
+    {
+        id: "3",
+        fileUrl: "/assets/demo.jpeg",
+        likes_count: 40,
+        bookmark_count: 50,
+        description: "Rich dad poor dad"
+    },
+    {
+        id: "4",
+        fileUrl: "/assets/demo.jpeg",
+        likes_count: 40,
+        bookmark_count: 50,
+        description: "Atomic Habits"
+    },
+    {
+        id: "5",
+        fileUrl: "/assets/demo.jpeg",
+        likes_count: 40,
+        bookmark_count: 50,
+        description: "Good to Great"
+    },
+
+];
+
+
+export default function Page() {
+    const { data: user } = useRetrieveUserQuery();
+    const [searchTerm, setSearchTerm] = useState("");
+
+    const filteredPosts = posts.filter(post =>
+        post.description.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    // const { lastJsonMessage } = useWebSocketContext();
+
+    // useEffect(() => {
+    //   console.log("Updated realtimeMessages:", lastJsonMessage);
+    //   refetch();
+    //   refetchTransactions();
+    //   refetchAccountAnalytics();
+    //   refetchTransactionAnalytics();
+    // }, [lastJsonMessage]);
+
+    if (!user) {
+        return;
+    }
+
+    return (
+        <div className="flex flex-col gap-5 w-full mt-12">
+            <div className="flex justify-between items-center mb-4">
+                <PageTitle title="Book Category" />
+                <div className="flex items-center gap-2">
+                    <Search size={32} className="hidden sm:block" />
+                    <Input
+                        type="text"
+                        placeholder="Search books..."
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                        className="w-[100] border border-gray-500 dark:border-white rounded p-2"
+                    />
+                </div>
+            </div>
+            <PostsGrid posts={filteredPosts} />
+        </div>
+    );
+}
