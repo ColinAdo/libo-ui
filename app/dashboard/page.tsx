b9380bc901ed4d70b31ed253b3983cb5766ff27c
ColinAdo <colinadodev30@gmail.com>
2025-02-19 17:59:33 +0300

    Add dynamic refresh

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,25 +1,25 @@
 "use client";
 
 import { useEffect, useState } from "react";
-// import { useWebSocketContext } from "@/hooks/WebSocketContext";
+import { useWebSocketContext } from "@/hooks/WebSocketContext";
 import { PostsGrid, PageTitle } from "@/components/dashboard";
 import { Search } from "lucide-react";
 import { Input } from "@/components/ui/input";
 import { useGetBooksQuery } from "@/redux/features/bookSlice";
 
 export default function Page() {
-  const { data: books } = useGetBooksQuery();
+  const { data: books, refetch } = useGetBooksQuery();
   const [searchTerm, setSearchTerm] = useState("");
-  // const { lastJsonMessage } = useWebSocketContext();
+  const { lastJsonMessage } = useWebSocketContext();
 
   const filteredPosts = books?.filter(book =>
     book.title.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
-  // useEffect(() => {
-  //   console.log("Updated realtimeMessages:", lastJsonMessage);
-  //   refetch();
-  // }, [lastJsonMessage]);
+  useEffect(() => {
+    console.log("Updated realtimeMessages:", lastJsonMessage);
+    refetch();
+  }, [lastJsonMessage]);
 
   return (
     <div className="flex flex-col gap-5 w-full mt-12">
