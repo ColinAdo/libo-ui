33ea7340e929dccfa82d1690119441401ed148fe
ColinAdo <colinadodev30@gmail.com>
2025-05-10 12:21:45 +0300

    Add Saved route

--- app/dashboard/[username]/saved/page.tsx
+++ app/dashboard/[username]/saved/page.tsx
@@ -1,35 +1,13 @@
 "use client";
 
-import { PageTitle, ProfileTabs } from "@/components/dashboard";
+import { useEffect, useState } from "react";
 import { PostsGrid } from "@/components/dashboard";
 import { Input } from "@/components/ui/input";
 import { Search } from "lucide-react";
-import { useRouter } from "next/router";
-import { useState } from "react";
-
-const posts = [
-    {
-        id: "1",
-        fileUrl: "/assets/demo.jpeg",
-        likes_count: 40,
-        bookmark_count: 50,
-        description: "Think and Grow Rich"
-    },
-    {
-        id: "2",
-        fileUrl: "/assets/demo.jpeg",
-        likes_count: 40,
-        bookmark_count: 50,
-        description: "Richest man in Babylon"
-    },
-    {
-        id: "3",
-        fileUrl: "/assets/demo.jpeg",
-        likes_count: 40,
-        bookmark_count: 50,
-        description: "Think like a Monk"
-    },
-];
+import { PageTitle } from "@/components/dashboard";
+import { ProfileTabs } from "@/components/dashboard";
+import { useGetBookmarkedBooksQuery } from "@/redux/features/bookSlice";
+import { useWebSocketContext } from "@/hooks/WebSocketContext";
 
 interface Props {
     params: {
@@ -39,15 +17,21 @@ interface Props {
 
 export default function Page({ params: { username } }: Props) {
     const [searchTerm, setSearchTerm] = useState("");
+    const { data: books, refetch } = useGetBookmarkedBooksQuery();
+    const { lastJsonMessage } = useWebSocketContext();
 
-    const filteredPosts = posts.filter(post =>
-        post.description.toLowerCase().includes(searchTerm.toLowerCase())
+    const filteredBooks = books?.filter(book =>
+        (book.description || "").toLowerCase().includes(searchTerm.toLowerCase())
     );
+
+    useEffect(() => {
+        refetch();
+    }, [lastJsonMessage]);
 
     return (
         <div className="p-4 mt-12">
             <div className="flex justify-between items-center mb-4">
-                <PageTitle title="Your Books" />
+                <PageTitle title="Your Bookmarked Books" />
                 <div className="flex items-center gap-2">
                     <Search size={32} className="hidden sm:block" />
                     <Input
@@ -60,7 +44,7 @@ export default function Page({ params: { username } }: Props) {
                 </div>
             </div>
             <ProfileTabs isCurrentUser={true} username={username} />
-            <PostsGrid posts={filteredPosts} />
+            <PostsGrid books={filteredBooks} />
         </div>
     );
 }
